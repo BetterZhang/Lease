5588909ba50b1ca19f7aead52cfa2d3b27d5b677
BetterZhang <betterzhang.dev@gmail.com>
2018-05-17 17:18:38 +0800

    UserDeviceService

---
+++ app/src/main/java/com/anshi/lease/service/UserDeviceApi.java
@@ -0,0 +1,71 @@
+package com.anshi.lease.service;
+
+import com.jme.common.network.DTResponse;
+import retrofit2.Call;
+import retrofit2.http.POST;
+
+/**
+ * Created by Android Studio.
+ * Author : zhangzhongqiang
+ * Email  : betterzhang.dev@gmail.com
+ * Time   : 2018/05/17 下午 5:09
+ * Desc   : description
+ */
+public interface UserDeviceApi {
+
+    /**
+     * 修改密码
+     * @return
+     */
+    @POST("/mobile/v1/device/modifypassword")
+    Call<DTResponse> modifypassword();
+
+
+    /**
+     * 根据ID获取车辆电池信息
+     * @return
+     */
+    @POST("/mobile/v1/device/getbypk")
+    Call<DTResponse> getByPK();
+
+
+    /**
+     * 根据ID获取车辆信息返回
+     * @return
+     */
+    @POST("/mobile/v1/device/getbypr")
+    Call<DTResponse> getByPR();
+
+
+    /**
+     * 根据车辆ID列表获取车辆信息列表
+     * @return
+     */
+    @POST("/mobile/v1/device/getlocbyvehiclepk")
+    Call<DTResponse> getLocByVehiclePK();
+
+
+    /**
+     * 根据车辆ID列表获取设备电量信息列表
+     * @return
+     */
+    @POST("/mobile/v1/device/getpowerbyvehiclepk")
+    Call<DTResponse> getPowerByVehiclePK();
+
+
+    /**
+     * 根据车辆ID以及时间区间获取车辆轨迹信息
+     * @return
+     */
+    @POST("/mobile/v1/device/gettrackbytime")
+    Call<DTResponse> getTrackByTime();
+
+
+    /**
+     * 根据当前登录用户查询当前用户下的所有车辆
+     * @return
+     */
+    @POST("/mobile/v1/device/getVehicleByUserId")
+    Call<DTResponse> getVehicleByUserId();
+
+}
---
+++ app/src/main/java/com/anshi/lease/service/UserDeviceService.java
@@ -0,0 +1,92 @@
+package com.anshi.lease.service;
+
+import com.anshi.lease.common.Constants;
+import com.jme.common.network.API;
+import com.jme.common.network.DTResponse;
+import com.jme.common.network.IService;
+import java.io.IOException;
+import java.util.HashMap;
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Call;
+
+/**
+ * Created by Android Studio.
+ * Author : zhangzhongqiang
+ * Email  : betterzhang.dev@gmail.com
+ * Time   : 2018/05/17 下午 2:04
+ * Desc   : description
+ */
+public class UserDeviceService extends IService<UserDeviceApi> {
+
+    public UserDeviceService() {
+        super(Constants.HttpConst.URL_BASE, true);
+    }
+
+    public static UserDeviceService getInstance() {
+        return (UserDeviceService) getInstance(UserDeviceService.class);
+    }
+
+    @Override
+    protected Interceptor addHeader() {
+        Interceptor interceptor = new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request request = chain.request();
+                return chain.proceed(request);
+            }
+        };
+        return interceptor;
+    }
+
+    public API modifypassword = new API<String>("modifypassword") {
+        @Override
+        public Call<DTResponse> request(HashMap<String, String> params) {
+            return mApi.modifypassword();
+        }
+    };
+
+    public API getByPK = new API<String>("getByPK") {
+        @Override
+        public Call<DTResponse> request(HashMap<String, String> params) {
+            return mApi.getByPK();
+        }
+    };
+
+    public API getByPR = new API<String>("getByPR") {
+        @Override
+        public Call<DTResponse> request(HashMap<String, String> params) {
+            return mApi.getByPR();
+        }
+    };
+
+    public API getLocByVehiclePK = new API<String>("getLocByVehiclePK") {
+        @Override
+        public Call<DTResponse> request(HashMap<String, String> params) {
+            return mApi.getLocByVehiclePK();
+        }
+    };
+
+    public API getPowerByVehiclePK = new API<String>("getPowerByVehiclePK") {
+        @Override
+        public Call<DTResponse> request(HashMap<String, String> params) {
+            return mApi.getPowerByVehiclePK();
+        }
+    };
+
+    public API getTrackByTime = new API<String>("getTrackByTime") {
+        @Override
+        public Call<DTResponse> request(HashMap<String, String> params) {
+            return mApi.getTrackByTime();
+        }
+    };
+
+    public API getVehicleByUserId = new API<String>("getVehicleByUserId") {
+        @Override
+        public Call<DTResponse> request(HashMap<String, String> params) {
+            return mApi.getVehicleByUserId();
+        }
+    };
+
+}
