cbd363286c039519a6eb93e57b3ac2ef273b66cd
chenwenwen <cjw19911217>
2018-06-07 21:51:04 +0800

    行车轨迹页面

--- app/src/main/java/com/anshi/lease/ui/activity/TrackActivity.java
+++ app/src/main/java/com/anshi/lease/ui/activity/TrackActivity.java
@@ -1,21 +1,7 @@
 package com.anshi.lease.ui.activity;
 
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 import com.anshi.lease.R;
-import com.anshi.lease.common.Constants;
-import com.anshi.lease.common.UserInfo;
-import com.anshi.lease.domain.UserVo;
-import com.anshi.lease.service.UserAuthService;
 import com.anshi.lease.ui.base.LeaseBaseActivity;
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
@@ -27,35 +13,19 @@ import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.model.LatLng;
-import com.jme.common.network.DTRequest;
-import com.jme.common.ui.base.ToolbarHelper;
-import com.squareup.picasso.Picasso;
-import java.util.HashMap;
 import butterknife.BindView;
 
 /**
  * Created by Android Studio.
  * Author : zhangzhongqiang
  * Email  : betterzhang.dev@gmail.com
- * Time   : 2018/05/15 下午 12:10
- * Desc   : App主页面
+ * Time   : 2018/06/07 下午9:37
+ * Desc   : 行车轨迹页面
  */
-public class MainActivity extends LeaseBaseActivity implements View.OnClickListener {
+public class TrackActivity extends LeaseBaseActivity {
 
-    @BindView(R.id.layout_drawer)
-    DrawerLayout layout_drawer;
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
-    @BindView(R.id.navigation_view)
-    NavigationView navigation_view;
     @BindView(R.id.mapView)
     MapView mapView;
-
-    ImageView iv_head;
-    TextView tv_name;
-    TextView tv_status;
-
-    private UserVo mUserVo;
 
     private BaiduMap mBaiduMap;
     public LocationClient mLocationClient;
@@ -65,130 +35,15 @@ public class MainActivity extends LeaseBaseActivity implements View.OnClickListe
 
     @Override
     protected int getContentViewId() {
-        return R.layout.activity_main;
+        return R.layout.activity_track;
     }
 
     @Override
     protected void initView() {
         super.initView();
-        initToolbar("小哥乐途", false);
-
-        setRightNavigation("", R.mipmap.ic_track, new ToolbarHelper.OnSingleMenuItemClickListener() {
-            @Override
-            public void onSingleMenuItemClick() {
-                startAnimActivity(TrackActivity.class);
-            }
-        });
-
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, layout_drawer, toolbar, R.string.navigation_drawer_open,
-                R.string.navigation_drawer_close);
-        layout_drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        View headerLayout = navigation_view.getHeaderView(0);
-        iv_head = headerLayout.findViewById(R.id.iv_head);
-        tv_name = headerLayout.findViewById(R.id.tv_name);
-        tv_status = headerLayout.findViewById(R.id.tv_status);
+        initToolbar("行车轨迹", true);
 
         initMap();
-    }
-
-    @Override
-    protected void initData(Bundle savedInstanceState) {
-        super.initData(savedInstanceState);
-        mUserVo = UserInfo.getInstance().getCurrentUser();
-        if (mUserVo != null)
-            setUserData();
-    }
-
-    private void setUserData() {
-        Picasso.with(this)
-                .load(Constants.HttpConst.URL_BASE_IMG + mUserVo.getKey_user_info().getUserIcon())
-                .placeholder(R.mipmap.ic_head_default)
-                .into(iv_head);
-        tv_name.setText(mUserVo.getKey_user_info().getNickName());
-        tv_status.setText(mUserVo.getKey_user_info().getUserRealNameAuthFlag().equals("AUTHORIZED") ? "已实名" : "未实名");
-    }
-
-    private void logout() {
-        HashMap<String, String> params = new HashMap<>();
-        sendRequest(UserAuthService.getInstance().logout, params, true);
-    }
-
-    @Override
-    protected void DataReturn(DTRequest request, String msgCode, Object response) {
-        super.DataReturn(request, msgCode, response);
-        switch (request.getApi().getName()) {
-            case "logout":
-                if (msgCode.equals("200")) {
-                    showShortToast("登出成功");
-                    startAnimActivity(LoginActivity.class);
-                    this.finish();
-                }
-                break;
-            default:
-                break;
-        }
-    }
-
-    private void gotoAuthIdCardActivity() {
-        startAnimActivity(AuthIdCardActivity.class);
-    }
-
-    @Override
-    protected void initListener() {
-        super.initListener();
-        navigation_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.item_bike:
-                        if (mUserVo.getKey_user_info().getUserRealNameAuthFlag().equals("AUTHORIZED"))
-                            gotoMyVehicleActivity();
-                        else
-                            gotoAuthTipActivity();
-                        break;
-                    case R.id.item_personal:
-                        gotoPersonalActivity();
-                        break;
-                    case R.id.item_editpwd:
-                        gotoEditPwdActivity();
-                        break;
-                    case R.id.item_logout:
-                        logout();
-                        break;
-                    default:
-                        break;
-                }
-                return false;
-            }
-        });
-        tv_status.setOnClickListener(this);
-    }
-
-    private void gotoAuthTipActivity() {
-        startAnimActivity(AuthTipActivity.class);
-    }
-
-    private void gotoMyVehicleActivity() {
-        startAnimActivity(MyVehicleActivity.class);
-    }
-
-    private void gotoPersonalActivity() {
-        startAnimActivity(PersonalActivity.class);
-    }
-
-    private void gotoEditPwdActivity() {
-        startAnimActivity(EditPwdActivity.class);
-    }
-
-    @Override
-    public void onClick(View view) {
-        if (mUserVo.getKey_user_info().getUserRealNameAuthFlag().equals("AUTHORIZED")) {
-            showShortToast("您已经实名认证，请勿重复提交");
-        } else {
-            gotoAuthIdCardActivity();
-        }
     }
 
     private void initMap() {
@@ -265,21 +120,21 @@ public class MainActivity extends LeaseBaseActivity implements View.OnClickListe
 
                 if (location.getLocType() == BDLocation.TypeGpsLocation) {
                     // GPS定位结果
-                    Toast.makeText(MainActivity.this, location.getAddrStr(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(TrackActivity.this, location.getAddrStr(), Toast.LENGTH_SHORT).show();
                 } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {
                     // 网络定位结果
-                    Toast.makeText(MainActivity.this, location.getAddrStr(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(TrackActivity.this, location.getAddrStr(), Toast.LENGTH_SHORT).show();
 
                 } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {
                     // 离线定位结果
-                    Toast.makeText(MainActivity.this, location.getAddrStr(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(TrackActivity.this, location.getAddrStr(), Toast.LENGTH_SHORT).show();
 
                 } else if (location.getLocType() == BDLocation.TypeServerError) {
-                    Toast.makeText(MainActivity.this, "服务器错误，请检查", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(TrackActivity.this, "服务器错误，请检查", Toast.LENGTH_SHORT).show();
                 } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
-                    Toast.makeText(MainActivity.this, "网络错误，请检查", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(TrackActivity.this, "网络错误，请检查", Toast.LENGTH_SHORT).show();
                 } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
-                    Toast.makeText(MainActivity.this, "手机模式错误，请检查是否飞行", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(TrackActivity.this, "手机模式错误，请检查是否飞行", Toast.LENGTH_SHORT).show();
                 }
             }
         }
@@ -304,4 +159,5 @@ public class MainActivity extends LeaseBaseActivity implements View.OnClickListe
         // 在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
         mapView.onPause();
     }
+
 }
