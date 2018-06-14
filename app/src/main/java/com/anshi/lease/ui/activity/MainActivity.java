509683d94b91ec1d9aa0122501a0c55b98872541
BetterZhang <cjw19911217>
2018-06-14 23:19:14 +0800

    车辆定位添加标记

--- app/src/main/java/com/anshi/lease/ui/activity/MainActivity.java
+++ app/src/main/java/com/anshi/lease/ui/activity/MainActivity.java
@@ -34,10 +34,6 @@ import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.map.MarkerOptions;
 import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.model.LatLng;
-import com.baidu.mapapi.search.geocode.GeoCodeResult;
-import com.baidu.mapapi.search.geocode.GeoCoder;
-import com.baidu.mapapi.search.geocode.OnGetGeoCoderResultListener;
-import com.baidu.mapapi.search.geocode.ReverseGeoCodeResult;
 import com.jme.common.network.DTRequest;
 import com.jme.common.ui.base.ToolbarHelper;
 import com.squareup.picasso.Picasso;
@@ -53,7 +49,7 @@ import butterknife.BindView;
  * Time   : 2018/05/15 下午 12:10
  * Desc   : App主页面
  */
-public class MainActivity extends LeaseBaseActivity implements View.OnClickListener, OnGetGeoCoderResultListener {
+public class MainActivity extends LeaseBaseActivity implements View.OnClickListener {
 
     private static final int accuracyCircleFillColor = 0xAAFFFF88;
     private static final int accuracyCircleStrokeColor = 0xAA00FF00;
@@ -92,7 +88,6 @@ public class MainActivity extends LeaseBaseActivity implements View.OnClickListe
     private MyLocationData locData;
 
     BitmapDescriptor mCurrentMarker;
-    GeoCoder mSearch = null; // 搜索模块，也可去掉地图模块独立使用
 
     @Override
     protected int getContentViewId() {
@@ -129,9 +124,6 @@ public class MainActivity extends LeaseBaseActivity implements View.OnClickListe
     @Override
     protected void initData(Bundle savedInstanceState) {
         super.initData(savedInstanceState);
-        // 初始化搜索模块，注册事件监听
-        mSearch = GeoCoder.newInstance();
-        mSearch.setOnGetGeoCodeResultListener(this);
         mUserVo = UserInfo.getInstance().getCurrentUser();
         if (mUserVo != null)
             setUserData();
@@ -358,16 +350,6 @@ public class MainActivity extends LeaseBaseActivity implements View.OnClickListe
         option.setCoorType("bd09ll"); // 设置坐标类型
         option.setScanSpan(1000);
         mLocationClient.setLocOption(option);
-    }
-
-    @Override
-    public void onGetGeoCodeResult(GeoCodeResult result) {
-
-    }
-
-    @Override
-    public void onGetReverseGeoCodeResult(ReverseGeoCodeResult result) {
-
     }
 
     /**
